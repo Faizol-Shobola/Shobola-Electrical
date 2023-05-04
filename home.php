14916d8b88327810db5425ed4e1c29ebe42e983d
shobola faruq <shobolafaruq@gmail.com>
2023-05-04 21:53:26 +0100

    READ blog posts to home page

---
+++ home.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The template for displaying Home page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package duromedia
+ */
+
+get_header();
+?>
+
+<?php
+get_footer();
