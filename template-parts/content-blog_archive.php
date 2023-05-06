06c4e252dee50a00ae56370abbeb2db24d0408a4
shobola faruq <shobolafaruq@gmail.com>
2023-05-06 18:48:22 +0100

    Update single blog page

--- template-parts/content-blog_archive.php
+++ template-parts/content-blog_archive.php
@@ -2,11 +2,11 @@
 <article class="post ttm-blog-classic clearfix">
   <!-- post-featured-wrapper -->
   <div class="ttm-post-featured-wrapper ttm-featured-wrapper">
-    <?php if(has_post_thumbnail()):?>                           
       <div class="ttm-post-featured">
+          <?php if(has_post_thumbnail()):?>                           
           <img class="img-fluid" src="<?php the_post_thumbnail_url();?>" alt="<?php the_title(); ?>">
+          <?php endif;?>
         </div>
-        <?php endif;?>
   </div><!-- post-featured-wrapper end -->
   <!-- ttm-blog-classic-content -->
   <div class="ttm-blog-classic-content">
@@ -22,12 +22,12 @@
       </div>
       <div class="entry-content">
           <div class="ttm-box-desc-text">
-              <p><?php the_content(); ?></p>
+              <p><?php the_excerpt(); ?></p>
           </div>
           <div class="ttm-blogbox-footer">
               <div class="post-meta">
                   <span class="ttm-meta-line byline"><i class="ti ti-user"></i>John Doe</span>
-                  <span class="ttm-meta-line tags-links"><i class="fa fa-comments-o"></i>0 Comments</span>
+                  <!-- <span class="ttm-meta-line tags-links"><i class="fa fa-comments-o"></i>0 Comments</span> -->
               </div>
               <a class="ttm-btn ttm-btn-size-md ttm-btn-color-dark btn-inline ttm-icon-btn-right" href="<?php the_permalink(); ?>">Read More<i class="fa fa-long-arrow-right"></i></a>
           </div>
---
+++ template-parts/content-post.php
@@ -0,0 +1,49 @@
+<div class="col-lg-8 content-area">
+  <!-- post -->
+  <article class="post ttm-blog-single clearfix">
+      <!-- post-featured-wrapper -->
+      <div class="ttm-post-featured-wrapper ttm-featured-wrapper">
+      <div class="ttm-post-featured">
+            <img class="img-fluid" src="<?php the_post_thumbnail_url();?>" alt="<?php the_title(); ?>">
+        </div>
+      </div>
+      <!-- post-featured-wrapper end -->
+      <!-- ttm-blog-classic-content -->
+      <div class="ttm-blog-single-content">
+          <div class="ttm-post-entry-header">
+              <div class="post-meta">
+                  <span class="ttm-meta-line byline"><i class="ti ti-user"></i>John Doe</span>
+                  <span class="ttm-meta-line tags-links"><i class="fa fa-comments-o"></i>0 Comments</span>
+              </div>
+          </div>
+          <div class="entry-content">
+              <div class="ttm-box-desc-text">
+                 <?php the_content(); ?>
+              </div>
+              <div class="social-media-block">
+                  <div class="d-sm-flex justify-content-between">
+                      <div class="ttm_tag_lists margin_top15">
+                          <span class="ttm-tags-links-title">
+                              Tags:</span>
+                          <span class="ttm-tags-links"><a href="blog-right-sidebar.html">Builders</a>
+                              <a href="blog-right-sidebar.html">Decor</a></span>
+                      </div>
+                      <div class="ttm-social-share-wrapper margin_top15">
+                          <h6 class="fs-16 m-0 padding_right10">Share : </h6>
+                          <ul class="social-icons">
+                              <li><a href="https://www.facebook.com/themetechMount18"><i class="fa fa-facebook"></i></a></li>
+                              <li><a href="https://twitter.com/themetechmount"><i class="fa fa-twitter"></i></a></li>
+                              <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                              <li><a href="https://www.linkedin.com/company/themetech-mount/"><i class="fa fa-linkedin"></i></a></li>
+                          </ul>
+                      </div>
+                  </div>
+              </div>
+              <div class="ttm-horizontal_sep width-100 margin_top30 margin_bottom30"></div>
+              
+              
+          </div>
+          
+      </div><!-- ttm-blog-classic-content end -->
+  </article><!-- post end -->
+</div>
