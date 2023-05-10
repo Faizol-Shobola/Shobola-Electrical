985f9fac3e63f0563e34324c39286a13aa5013e6
Shobola Faizol <shobolafaizol@gmail.com>
2023-05-10 16:19:13 +0100

    Update project post type to display category for each post

--- template-parts/content-project.php
+++ template-parts/content-project.php
@@ -6,7 +6,7 @@
                 <div class="col-lg-12">
                     <div class="ttm-tabs ttm-tab-style-03">
                         <div class="d-lg-flex align-items-center justify-content-between">
-                            <div class="ttm-bg ttm-col-bgcolor-yes ttm-bgcolor-grey ttm-bg ttm-left-span res-991-padding_top50 res-991-padding_right30  res-991-padding_bottom30 res-991-mt_40  res-991-mr_20">
+                            <div class="ttm-bg ttm-col-bgcolor-yes ttm-bg ttm-left-span res-991-padding_top50 res-991-padding_right30  res-991-padding_bottom30 res-991-mt_40  res-991-mr_20">
                                 <div class="ttm-col-wrapper-bg-layer ttm-bg-layer spacing-1">
                                     <div class="ttm-col-wrapper-bg-layer-inner"></div>
                                 </div>
@@ -22,25 +22,32 @@
                             </div>
                             <ul class="tabs portfolio-filter res-991-padding_bottom15 res-991-padding_top30">
                                 <li class="tab active"><a href="#" data-filter="*"> All </a></li>
-                                <li class="tab"><a href="#" data-filter=".Buildings">Buildings</a></li>
-                                <li class="tab"><a href="#" data-filter=".Houses">Houses</a></li>
-                                <li class="tab"><a href="#" data-filter=".Interior">Interior</a></li>
-                                <li class="tab"><a href="#" data-filter=".Offices">Offices</a></li>
                             </ul>
                         </div>
                         <div class="content-tab">
                         <?php $query_args = array(
-                            'post_type' => 'projects',
+                            'post_type' => 'project',
                             'posts_per_page' => 6,
-                            'order' => 'rand',
+                            'order' => 'desc',
                         );
+                        $query = new WP_Query( $query_args );
                         ?>
 
+                        <?php if( $query->have_posts() ) :  ?>
+                            
                             <!-- content-inner -->
                             <div class="row isotope-project">
-                                <?php $query = new WP_Query( $query_args );
-                                if( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
-                                <div class="col-lg-4 col-md-4 col-sm-6 project_item Buildings Interior">
+                                <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+                                <?php $terms = get_the_terms( $post->ID , 'project_category' );
+                                if ( is_array( $terms ) && ! is_wp_error( $terms ) ) {
+                                    foreach ($terms as $term) {
+                                        $term_link = get_term_link($term, 'project_category');
+                                        if (is_wp_error($term_link))
+                                            continue;
+                                    }
+                                }
+                                ?>
+                                <div class="<?php echo 'col-lg-4 col-md-4 col-sm-6 project_item' . ' ' . $term->name; ?>">
                                     <!-- featured-imagebox-portfolio -->
                                     <div class="featured-imagebox featured-imagebox-portfolio style1">
                                         <!-- ttm-box-view-overlay -->
@@ -60,19 +67,26 @@
                                                 <h3><a href="<?php the_permalink(); ?>"><?php echo wp_trim_words( get_the_title(), 10 ); ?></a></h3>
                                             </div>
                                             <span class="category">
-                                            <?php
-                                                $categories = get_the_category();
-                                                if ( !empty( $categories ) ):
-                                                foreach($categories as $cat): 
-                                            ?>
-                                                <span><?php echo $cat->name;?></span>,
-										    <?php endforeach; endif;?>
+                                                <?php $terms = get_the_terms( $post->ID , 'project_category' );
+                                                if ( is_array( $terms ) && ! is_wp_error( $terms ) ) {
+                                                    foreach ($terms as $term) {
+                                                        $term_link = get_term_link($term, 'project_category');
+                                                        if (is_wp_error($term_link))
+                                                            continue;
+                                                        echo '<span>' . $term->name . '</span> ';
+                                                    }
+                                                }
+                                                ?>
                                             </span>
                                         </div>
                                     </div><!-- featured-imagebox-portfolio -->
                                 </div>
-                                <?php endwhile; else: endif; ?>
+                            <?php endwhile; ?>
                             </div>
+                        <?php wp_reset_postdata(); ?>
+						<?php else : ?>
+							<p><?php esc_html_e( 'Sorry, no project is available.' ); ?></p>
+						<?php endif; ?>
                         </div>
                     </div>
                 </div>
