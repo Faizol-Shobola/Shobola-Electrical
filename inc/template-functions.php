b7b77d610525eb2865dc56918b03860b383a2652
shobola faruq <shobolafaruq@gmail.com>
2023-05-10 21:03:19 +0100

    Refactor testimonial custom post type function

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -35,3 +35,41 @@ function shobola_electrical_pingback_header() {
 	}
 }
 add_action( 'wp_head', 'shobola_electrical_pingback_header' );
+
+
+//custom post (testimonials)
+function testimonial_init() {
+	// set up testimonial labels
+	$labels = array(
+			'name' => 'Testimonials',
+			'singular_name' => 'testimonial',
+			'add_new' => 'Add New Testimonial',
+			'add_new_item' => 'Add New Testimonial',
+			'edit_item' => 'Edit Testimonial',
+			'new_item' => 'New Testimonial',
+			'all_items' => 'All Testimonials',
+			'view_item' => 'View Testimonial',
+			'search_items' => 'Search Testimonials',
+			'not_found' =>  'No Testimonials Found',
+			'not_found_in_trash' => 'No Testimonial found in Trash', 
+			'parent_item_colon' => '',
+			'menu_name' => 'Testimonials',
+	);
+	
+	// register post type
+	$args = array(
+			'labels' => $labels,
+			'public' => true,
+			'has_archive' => true,
+			'show_ui' => true,
+			'capability_type' => 'post',
+			'hierarchical' => false,
+			'rewrite' => array('slug' => 'testimonial'),
+			'query_var' => true,
+			'menu_icon' => 'dashicons-businessman',
+			'supports' => array('title','editor','excerpt','trackbacks','custom-fields',
+													'comments','revisions','thumbnail','author','page-attributes')
+	);
+	register_post_type( 'testimonial', $args );
+}
+	add_action( 'init', 'testimonial_init' );
